4238366ebb85dabeb2e74caaf7aa27e59dc28349
Ashihad <grzegorz.esser@gmail.com>
2024-10-20 11:56:56 +0200

    Added test

--- tests/test_linalg.cpp
+++ tests/test_linalg.cpp
@@ -1,9 +1,17 @@
 #include <gtest/gtest.h>
+#include "gtest_common_define.hpp"
+#include "linalg.hpp"
 
-// Demonstrate some basic assertions.
-TEST(linalgTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#include <vector>
+
+class LinalgTest : public testing::Test {};
+
+TEST_F(LinalgTest, scalar_product_basic) {
+  // Primitive generator test, TODO: make something real
+  double example_x_vector[] = {1, -2, 3, -4, 5, -6};
+  double example_y_vector[] = {7, 1/8., 9, 1/10., 11, 1/12.};
+  std::size_t vec_size = 6ul;
+
+  double result = scalar_product_x_y(vec_size, example_x_vector, example_y_vector);
+  EXPECT_DOUBLE_EQ(result, 87.85);
 }
